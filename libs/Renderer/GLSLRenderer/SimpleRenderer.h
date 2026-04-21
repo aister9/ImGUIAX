a531bcfd2011ba20f41b585a7080782982623e26
영우 김 <aister9@koreatech.ac.kr>
2026-04-22 04:33:02 +0900

    Remove glm extension instantly

--- libs/Renderer/GLSLRenderer/SimpleRenderer.h
+++ libs/Renderer/GLSLRenderer/SimpleRenderer.h
@@ -1,13 +1,6 @@
 #pragma once
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
-#include <glm/gtx/normal.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/vector_angle.hpp>
-#include <glm/gtx/string_cast.hpp>
 
 #include "3rdParty/helper_math.h"
 #include <vector>
